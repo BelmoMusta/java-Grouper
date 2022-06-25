66911a80ce95a7b4d1bc315f6f0163704b56d97c
Mustapha <belmokhtar.91@gmail.com>
2022-06-25 13:11:53 +0100

    enhance grouper

--- src/main/java/org/mustabelmo/grouper/Grouper.java
+++ src/main/java/org/mustabelmo/grouper/Grouper.java
@@ -1,6 +1,8 @@
 package org.mustabelmo.grouper;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Optional;
 
@@ -10,7 +12,7 @@ import java.util.Optional;
  * @param <K> The key type
  * @param <V> the values type
  */
-public abstract class Grouper<K, V> {
+public abstract class Grouper<K, V> implements Iterable<V>{
     private final Map<K, Collection<V>> internalMap;
     private final GroupingStrategy<K, V> groupingStrategy;
 
@@ -28,6 +30,26 @@ public abstract class Grouper<K, V> {
         collection.add(value);
         return value;
     }
+    
+    public Collection<V> putAll(K key, Collection<V> values) {
+        Collection<V> collection = internalMap.get(key);
+        if (collection == null) {
+            collection = groupingStrategy.getCollection();
+            internalMap.put(key, collection);
+        }
+        collection.addAll(values);
+        return values;
+    }
+    
+    public Collection<K> findGroupsOf(V value) {
+        Collection<K> groupsOfV = new ArrayList<>();
+        internalMap.forEach((k,v) -> {
+            if (v.contains(value)){
+                groupsOfV.add(k);
+            }
+        });
+        return groupsOfV;
+    }
 
     public Collection<V> get(K key) {
         return Optional.ofNullable(internalMap.get(key))
@@ -41,7 +63,32 @@ public abstract class Grouper<K, V> {
 
     public Collection<V> reduce() {
         Collection<V> collection = groupingStrategy.getCollection();
-        internalMap.forEach((k, v) -> collection.addAll(v));
+        reduceTo(collection);
         return collection;
+    }
+    
+    public void reduceTo(Collection<V> collection) {
+        internalMap.forEach((k, v) -> collection.addAll(v));
+    }
+    private static class InternalIterator<R> implements Iterator<R> {
+        final Iterator<R> innerIterator;
+    
+        private InternalIterator(Collection<R> innerCollection) {
+            innerIterator = innerCollection.iterator();
+        }
+    
+        @Override
+        public boolean hasNext() {
+            return innerIterator.hasNext();
+        }
+    
+        @Override
+        public R next() {
+            return innerIterator.next();
+        }
+    }
+    @Override
+    public Iterator<V> iterator() {
+        return new InternalIterator<>(reduce());
     }
 }
--- src/test/java/org/mustabelmo/grouper/tests/TestGrouper.java
+++ src/test/java/org/mustabelmo/grouper/tests/TestGrouper.java
@@ -5,7 +5,11 @@ import org.junit.Test;
 import org.mustabelmo.grouper.ArrayListGrouper;
 import org.mustabelmo.grouper.Grouper;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 public class TestGrouper {
     @Test
@@ -17,7 +21,8 @@ public class TestGrouper {
         grouper.put(1, "c");
         grouper.put(2, "c");
         Assert.assertEquals(4, grouper.get(1).size());
-        Assert.assertEquals(4, grouper.get(0).size());
+        Assert.assertEquals(1, grouper.get(2).size());
+        
     }
 
     @Test
@@ -33,6 +38,50 @@ public class TestGrouper {
         Assert.assertTrue(reduce.contains("b"));
         Assert.assertTrue(reduce.contains("c"));
 
+    }
+    
+    @Test
+    public void testIterator() {
+        Grouper<Integer, String> grouper = new ArrayListGrouper<>();
+        grouper.put(1, "a");
+        grouper.put(1, "b");
+        grouper.put(1, "c");
+        grouper.put(1, "c");
+        grouper.put(2, "c");
+        Iterator<String> iterator = grouper.iterator();
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertEquals("a", iterator.next());
+        Assert.assertEquals("b", iterator.next());
+        Assert.assertEquals("c", iterator.next());
+        Assert.assertEquals("c", iterator.next());
+        Assert.assertEquals("c", iterator.next());
+        Assert.assertFalse(iterator.hasNext());
+    
+    }
+    @Test
+    public void testPutAll() {
+        Grouper<Integer, String> grouper = new ArrayListGrouper<>();
+        grouper.putAll(1, Arrays.asList("a", "b", "c"));
+        List<String> reduced = new ArrayList<>();
+        grouper.reduceTo(reduced);
+        Assert.assertEquals(3, reduced.size());
+        Assert.assertEquals("a", reduced.get(0));
+        Assert.assertEquals("b", reduced.get(1));
+        Assert.assertEquals("c", reduced.get(2));
+    }
+    
+    @Test
+    public void testFindGroupsOf() {
+        Grouper<Integer, String> grouper = new ArrayListGrouper<>();
+        grouper.putAll(1, Arrays.asList("a", "b", "c"));
+        grouper.putAll(2, Arrays.asList("a", "e", "f"));
+        Collection<Integer> groupsOfA = grouper.findGroupsOf("a");
+        Iterator<Integer> iterator = groupsOfA.iterator();
+        Assert.assertEquals(2, groupsOfA.size());
+        Assert.assertEquals(Integer.valueOf(1), iterator.next());
+        Assert.assertEquals(Integer.valueOf(2), iterator.next());
+        System.out.println(grouper);
+        
     }
 
 }
